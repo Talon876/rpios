fa411807c938fcace883cf962bd51e86011388f2
Talon Daniels <talondaniels@gmail.com>
2014-03-29 17:37:50 -0600

    reads in pattern and blinks SOS signal

--- source/main.s
+++ source/main.s
@@ -20,34 +20,41 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+//load pattern in to r4 and set r5 to 0
+ptrn .req r4
+ldr ptrn, =pattern
+ldr ptrn,[ptrn]
+seq .req r5
+mov seq, #0
+
 loop$:
-    //turn on OK led by setting pin to off (yes, off)
-    pinNum .req r0
-    pinVal .req r1
-    mov pinNum, #16
-    mov pinVal, #0 //0 = off, not 0 = on
+    
+    //set r1 to pattern[seq]
+    mov r1, #1
+    lsl r1, seq
+    and r1, ptrn
+
+    //turn OK led  if we're on a 1, otherwise turn it off
+    mov r0, #16 //pin number
+    //pin value is set from pattern
     bl SetGpio
-    .unreq pinNum
-    .unreq pinVal
 
-    //wait 0.13s
-    ldr r0, =130000
+    add seq, #1 //add one to the sequence
+    cmp seq, #32 //see if we're higher than 32
+    movhi seq, #0 //if we are higher than 32, set to 0
+
+    //wait 0.25s
+    ldr r0, =250000
     bl Sleep
-
-    //turn off OK led 
-    pinNum .req r0
-    pinVal .req r1
-    mov pinNum, #16
-    mov pinVal, #1
-    bl SetGpio
-    .unreq pinNum
-    .unreq pinVal
-
-    //wait 0.15s
-    ldr r0, =150000
-    bl Sleep
-
-    str r1,[r0,#40] //store in 40 offset from controller (turn off)
 
     b loop$
+
+.section .data
+
+.align 2 //.align num ensures next line is a multiple of 2^num, in this case: 4
+
+//used to represent morse code
+//a 0 is led off, a 1 is led on, it is read from right to left
+pattern:
+.int 0b11111111101010100010001000101010
 
