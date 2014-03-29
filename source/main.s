7336e5f7e8fded72af4fc5a746de769dfd6dc727
Talon Daniels <talondaniels@gmail.com>
2014-03-29 10:17:33 -0600

    starting over so i can test it as i go on rpi

--- source/main.s
+++ source/main.s
@@ -4,48 +4,4 @@
 .globl _start
 
 _start:
-    b main
 
-    .section .text
-
-    main:
-    mov sp,#0x8000
-
-
-
-    //blink forever
-    loop$:
-
-        //turn led on
-        pinNum .req r0
-        pinFunc .req r1
-        mov pinNum,#16
-        mov pinFunc,#0
-        bl SetGpioFunction
-        .unreq pinNum
-        .unreq pinFunc
-
-        //wait a bit
-        mov r2,#0x3F0000
-        wait1$:
-            sub r2, #1
-            cmp r2, #0
-            bne wait1$
-
-        //turn led off
-        pinNum .req r0
-        pinFunc .req r1
-        mov pinNum,#16
-        mov pinFunc,#1
-        bl SetGpioFunction
-        .unreq pinNum
-        .unreq pinFunc
-
-        //wait a bit
-        mov r2,#0x3F0000
-        wait2$:
-            sub r2, #1
-            cmp r2, #0
-            bne wait2$
-
-    b loop$
